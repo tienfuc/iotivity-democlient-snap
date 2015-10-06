8c28c9db853dc695ed999d97352bcadebb733045
Gerald Yang <gerald.yang@canonical.com>
2015-10-06 11:40:13 +0800

    Modify build and program path.

--- my_program.sh
+++ my_program.sh
@@ -1,2 +1,2 @@
 sudo stty -F /dev/ttyACM0 speed 1200 cs8 -cstopb -parenb
-sudo /home/gerald/canonical/iot/arduino/arduino-1.5.7/hardware/tools/bossac -i -d --port=ttyACM0 -U false -e -w -v -b /home/gerald/canonical/iot/arduino/iotivity/out/arduino/arm/release/DemoServer.hex -R
+sudo /home/gerald/canonical/iot/arduino/arduino-1.5.7/hardware/tools/bossac -i -d --port=ttyACM0 -U false -e -w -v -b /home/gerald/canonical/iot/iotivity/out/arduino/arm/release/DemoServer.hex -R
